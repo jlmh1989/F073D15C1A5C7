e35ea9cb655ec87245a1323cb3f9fec62ab5b495
ramitto <ramiro.acevedo.navarro@gmail.com>
2014-06-09 14:40:53 -0500

    Revert "Es una prueba de un desarrollo"
    This reverts commit 0c7e7016713ca60f21120be9b3f47ba8ac1b83fc.

--- protected/config/test.php
+++ protected/config/test.php
@@ -3,9 +3,7 @@
 return CMap::mergeArray(
 	require(dirname(__FILE__).'/main.php'),
 	array(
-            
 		'components'=>array(
-                    
 			'fixture'=>array(
 				'class'=>'system.test.CDbFixtureManager',
 			),
