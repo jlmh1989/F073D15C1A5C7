adecd026689ecf5100caeba62449e9d5c96d816e
joseluis <joseluis@192.168.0.5>
2014-09-20 20:53:10 -0500

    Commit Prueba

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
+//codigo modificado
 require_once($yii);
 Yii::createWebApplication($config)->run();
