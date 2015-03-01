44c92c109abbc7f1ea0ce076c1be6bb07117c56f
jlmh1989 <jlmh1989@gmail.com>
2015-03-01 13:31:17 -0600

    Se agrego columna maestro en index materiales prestandos

--- protected/views/teachers/loanMaterial/index.php
+++ protected/views/teachers/loanMaterial/index.php
@@ -17,7 +17,39 @@ if(Yii::app()->user->getState("rol") === constantes::ROL_ADMIN_SISTEMA){
 <h1><?= $titulo ?></h1>
 
 <?php 
-$this->widget('zii.widgets.grid.CGridView', array(
+if(Yii::app()->user->getState("rol") === constantes::ROL_ADMIN_SISTEMA){
+    $this->widget('zii.widgets.grid.CGridView', array(
+	'dataProvider'=>$data,
+        'columns'=> array(
+            array('name'=>'fkCatMaterial',
+                'header'=>'Nombre Material',
+                'type'=>'raw',
+                'value'=>'$data->fkCatMaterialDetail->fkCatMaterial->desc_material'),
+            array('name'=>'material_code',
+                'header'=>'C&oacute;digo Material',
+                'type'=>'raw',
+                'value'=>'$data->fkCatMaterialDetail->material_code'),
+            array('name'=>'fkCourse',
+                'header'=>'Curso',
+                'type'=>'raw',
+                'value'=>'$data->fkCourse->desc_curse'),  
+            array('name'=>'pick_date',
+                'header'=>'Fecha de pr&eacute;stamo',
+                'type'=>'raw',
+                'value'=>'$data->pick_date'),  
+            array('name'=>'drop_date',
+                'header'=>'Fecha l&iacute;mite de entrega',
+                'type'=>'raw',
+                'value'=>'$data->drop_date'),  
+            array('name'=>'fkTeacher',
+                'header'=>'Nombre Maestro',
+                'type'=>'raw',
+                'value'=>'$data->fkTeacher->name'),  
+            'comment'
+        ),
+));
+}else{
+    $this->widget('zii.widgets.grid.CGridView', array(
 	'dataProvider'=>$data,
         'columns'=> array(
             array('name'=>'fkCatMaterial',
@@ -43,4 +75,5 @@ $this->widget('zii.widgets.grid.CGridView', array(
             'comment'
         ),
 ));
+}
 ?>
